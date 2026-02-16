c632ab1ab492120da09f6ad19354be92f62854ed
safiabout <safiaboutalebab@gmail.com>
2026-02-16 15:33:34 -0800

    add debug statement in the correct spot

--- system/basic/Worker.h
+++ system/basic/Worker.h
@@ -342,6 +342,11 @@ public:
         //send vertices according to hash_id (reduce)
         sync_graph();
 
+        // ---- DEBUG: print vertex distribution ----
+        cout << "Rank " << _my_rank 
+            << " vertex count after sync_graph: " 
+            << vertexes.size() << endl;
+
         message_buffer->init(vertexes);
         //barrier for data loading
         worker_barrier(); //@@@@@@@@@@@@@
@@ -479,13 +484,6 @@ public:
 
         //send vertices according to hash_id (reduce)
         sync_graph();
-
-
-        // ---- DEBUG: print vertex distribution ----
-        cout << "Rank " << _my_rank 
-            << " vertex count after sync_graph: " 
-            << vertexes.size() << endl;
-            
         message_buffer->init(vertexes);
         //barrier for data loading
         worker_barrier(); //@@@@@@@@@@@@@
