10be6f7c4795bf254a08a00fcfc6d32fbafd97d7
safiabout <safiaboutalebab@gmail.com>
2026-03-01 09:38:31 -0800

    change dir for train txt file in bash script

--- sssp/source_run.sh
+++ sssp/source_run.sh
@@ -2,4 +2,4 @@ while read src; do
   mpiexec.openmpi -n 16 --oversubscribe --hostfile ~/hosts \
     -x CLASSPATH -x LD_LIBRARY_PATH -x JAVA_HOME \
     ./run $src /largeTwitchFolder /outputLargeTwitchFolder_$src
-done < train_nodes.txt
+done < train_test/selected_nodes_64_train0.20_seed0_train.txt
