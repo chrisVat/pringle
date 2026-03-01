9a6043bac3105225caf4aaeb4537da90ca8112da
safiabout <safiaboutalebab@gmail.com>
2026-03-01 09:50:51 -0800

    add ability to add header to vertex comm file and redirect mpiexec's stdin to /dev/null to read multiple nodes

--- sssp/comm_trace_utils.cpp
+++ sssp/comm_trace_utils.cpp
@@ -6,6 +6,15 @@
 using namespace std;
 
 void merge_worker_files(int source, int num_workers) {
+    bool write_header = false;
+
+    // Check if file already exists
+    ifstream check("vertex_comm_all.csv");
+    if(!check.good()) {
+        write_header = true;
+    }
+    check.close();
+
     ofstream global("vertex_comm_all.csv", ios::app);
 
     for(int rank = 0; rank < num_workers; rank++) {
@@ -17,8 +26,13 @@ void merge_worker_files(int source, int num_workers) {
 
         string line;
 
-        // skip header
-        getline(in, line);
+        // Read header from worker
+        if(getline(in, line)) {
+            if(write_header) {
+                global << line << "\n";
+                write_header = false;
+            }
+        }
 
         while(getline(in, line)) {
             global << line << "\n";
--- sssp/source_run.sh
+++ sssp/source_run.sh
@@ -1,5 +1,6 @@
 while read src; do
   mpiexec.openmpi -n 16 --oversubscribe --hostfile ~/hosts \
     -x CLASSPATH -x LD_LIBRARY_PATH -x JAVA_HOME \
-    ./run $src /largeTwitchFolder /outputLargeTwitchFolder_$src
+    ./run $src /largeTwitchFolder /outputLargeTwitchFolder_$src \
+    < /dev/null
 done < /home/ubuntu/pringle/train_test/selected_nodes_64_train0.20_seed0_train.txt
