8929f337f975e2f3cff6ef069357a9aa97cf908c
safiabout <safiaboutalebab@gmail.com>
2026-03-04 14:29:49 -0800

    add query timing

--- sssp/source_run.sh
+++ sssp/source_run.sh
@@ -1,6 +1,11 @@
+QUERIES_START=$(date +%s%N)
 while read src; do
   mpiexec.openmpi -n 16 --oversubscribe --hostfile ~/hosts \
     -x CLASSPATH -x LD_LIBRARY_PATH -x JAVA_HOME \
     ./run $src /largeTwitchFolder /outputLargeTwitchFolder_$src \
     < /dev/null
 done < /home/ubuntu/pringle/train_test/selected_nodes_64_train0.20_seed0_train.txt
+QUERIES_END=$(date +%s%N)
+ELAPSED_MS=$(( (QUERIES_END - QUERIES_START) / 1000000 ))
+echo "Total query time: ${ELAPSED_MS}ms"
+echo "Total query time: ${ELAPSED_MS}ms" >> ~/query_times.log
